13e0281370dde90de9d3426c71a6384255f96433
lichaa824 <lisandrorubi@gmail.com>
2023-06-16 19:55:36 -0300

    add auth form

---
+++ messenger-frontend/app/(root)/page.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import AuthForm from './components/AuthForm';
+
+export default function Auth() {
+  return (
+    <div
+      className='
+        flex 
+        min-h-full 
+        flex-col 
+        justify-center 
+        py-12 
+        sm:px-6 
+        lg:px-8 
+        bg-gray-100
+      '
+    >
+      <div className='mx-3 sm:mx-auto sm:w-full sm:max-w-md'>
+        <Image
+          height='48'
+          width='48'
+          className='mx-auto w-auto'
+          src='/images/messengerlogo.png'
+          alt='Logo'
+        />
+        <h2
+          className='
+            mt-6 
+            text-center 
+            text-3xl 
+            font-bold 
+            tracking-tight 
+            text-gray-900
+          '
+        >
+          Sign in to your account
+        </h2>
+      </div>
+        <AuthForm />
+    </div>
+  );
+}
