25759c80befbee62ba65fa1acdc130172c13dbbb
lichaa824 <lisandrorubi@gmail.com>
2023-07-04 13:15:32 -0300

    Add default avatar image and name

--- messenger-frontend/app/home/components/Sidebar/ConversationBadge.tsx
+++ messenger-frontend/app/home/components/Sidebar/ConversationBadge.tsx
@@ -1,22 +1,58 @@
-// interface ConversationBadgeProps{
-//     AvatarImage: string;
-//     Name: string;
-// }
+'use client';
 
-interface TestProps {
+import { useEffect, useState } from 'react';
+
+interface ConversationBadgeProps {
   id: string;
   onSelect: (value: string) => void;
 }
 
-export default function ConversationBadge({ id, onSelect }: TestProps) {
+interface Information {
+  name: string;
+  avatarUrl: string;
+}
+
+export default function ConversationBadge({
+  id,
+  onSelect,
+}: ConversationBadgeProps) {
+  const [information, setInformation] = useState<Information>();
+
+  useEffect(() => {
+    const fetchInformation = async () => {
+      const res = await fetch(
+        `${
+          process.env.NEXT_PUBLIC_BACKEND_SERVER as string
+        }/api/chat/getInfoOfConversation/${id}`,
+        { credentials: 'include', method: 'GET', cache: 'default' }
+      );
+      const resInfo = await res.json();
+      setInformation(resInfo);
+    };
+    fetchInformation();
+  }, [id]);
+
   return (
-    <div className='h-22 max-w-max bg-slate-600'>
+    <div className='max-w-max bg-slate-200 rounded-lg'>
       <button
         onClick={() => {
           onSelect(id);
         }}
+         className='flex items-center p-8 h-12 '
       >
-        Id de conversacion: {id}
+        <div>
+          {information?.avatarUrl || (
+            <div className='avatar'>
+              <div className='h-12 rounded-full'>
+                <img
+                  src='https://static-00.iconduck.com/assets.00/avatar-default-symbolic-icon-2048x1949-pq9uiebg.png'
+                  alt='default avatar'
+                />
+              </div>
+            </div>
+          )}
+        </div>
+        {information && information.name}
       </button>
     </div>
   );
