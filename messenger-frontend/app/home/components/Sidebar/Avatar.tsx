3cface2aedb44286e94a9ad8a63b8be7cf0dd75d
lichaa824 <lisandrorubi@gmail.com>
2023-07-10 19:27:15 -0300

    add avatar component

---
+++ messenger-frontend/app/home/components/Sidebar/Avatar.tsx
@@ -0,0 +1,23 @@
+// import socket from "../../socket/socket";
+// import { useEffect } from "react";
+
+
+// function Avatar() {
+
+//     useEffect(() => {
+//         socket.on(, () => {})
+//     },[])
+
+
+
+//   return (
+//     <div className='avatar offline'>
+//       <div className='h-8 rounded-full ring  ring-offset-base-100 ring-offset-2'>
+//         <img
+//           src='https://static-00.iconduck.com/assets.00/avatar-default-symbolic-icon-2048x1949-pq9uiebg.png'
+//           alt='default avatar'
+//         />
+//       </div>
+//     </div>
+//   );
+// }
