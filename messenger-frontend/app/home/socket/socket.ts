f20f778b3efab90a2210bc5f18fea9652baf74c0
lichaa824 <lisandrorubi@gmail.com>
2023-07-11 18:17:40 -0300

    fix socket error on deploy

--- messenger-frontend/app/home/socket/socket.ts
+++ messenger-frontend/app/home/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3002';
+const URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BACKEND_SERVER as string : 'http://localhost:3002';
 const socket = io(URL, {withCredentials: true});
 
 export default socket;
